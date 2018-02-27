7d294cec9ff24c9b79c3d31165899536fa19c790
Sun-Q <sunq@edgehill.ac.uk>
2018-02-27 14:00:41 +0000

    Add unit test for Add()

---
+++ UnitTestingTests/MathToolTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting.Tests
+{
+    [TestClass()]
+    public class MathToolTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            // Arrange
+            int num1 = 10;
+            int num2 = 23;
+            int expected = 33;
+            int sum;
+
+            // Act
+            sum = MathTool.Add(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expected, sum);
+        }
+    }
+}
